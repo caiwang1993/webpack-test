a5589b8c9fc3a37fe807b6d901293bf09f20a026
caiwang0921 <505673793@qq.com>
2020-07-01 16:27:04 +0800

    webpack配置

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,8 +1,22 @@
 /**
  * Created by cw on 2020/6/28.
  */
+/*const config = require('./config.json')*/
+import React,{Component} from 'react'
+import config from './config.json'
+/*
 module.exports = function () {
     let greet = document.createElement('div');
-    greet.textContent = 'hi three and greetings';
+    greet.textContent = config.greetText;
     return greet
-};
+};*/
+class Greeter extends Component{
+    render(){
+        return(
+            <div>
+            {config.greetText}
+            </div>
+        );
+    }
+}
+export default Greeter
--- app/main.js
+++ app/main.js
@@ -1,5 +1,12 @@
 /**
  * Created by cw on 2020/6/28.
  */
-const greeter = request('./Greeter.js');
-document.querySelecter('#root').appendChild(greeter);
+/*
+const greeter = require('./Greeter.js');
+document.querySelector('#root').appendChild(greeter());*/
+import React from 'react'
+import {render} from 'react-dom'
+import Greeter from './Greeter'
+import './main.css'
+
+render(<Greeter />,document.getElementById('root'));
