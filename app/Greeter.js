f9d69e117343f22275d885e4db32ccaade8c396b
caiwang0921 <505673793@qq.com>
2020-07-01 11:14:17 +0800

    webpack配置

---
+++ app/Greeter.js
@@ -0,0 +1,8 @@
+/**
+ * Created by cw on 2020/6/28.
+ */
+module.exports = function () {
+    let greet = document.createElement('div');
+    greet.textContent = 'hi three and greetings';
+    return greet
+};
---
+++ app/main.js
@@ -0,0 +1,5 @@
+/**
+ * Created by cw on 2020/6/28.
+ */
+const greeter = request('./Greeter.js');
+document.querySelecter('#root').appendChild(greeter);
